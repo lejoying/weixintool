d817ce2835556cfb2a9e1e2b9f438842e202a77d
wsds <fengzhicheng@lejoying.com>
2013-05-08 15:41:44 +0800

    The server's folder structure has been modified.
    The bind server has been created, without test.

---
+++ server/bindserver/requestHandlers.js
@@ -0,0 +1,21 @@
+/**
+ * Date: 2013.04.15
+ * requestHandles dispatch.
+ */
+
+var requestHandlers = {};
+
+var globaldata = root.globaldata;
+
+var bind = require('./handlers/bind');
+
+requestHandlers.bind = function (request, response, pathObject, getParam) {
+    var echostr = getParam["echostr"];
+    bind.message(echostr, response);
+};
+
+requestHandlers.test = function (request, response, pathObject, getParam) {
+    response.write(JSON.stringify({a: 1, b: 2}));
+};
+
+module.exports = requestHandlers;
