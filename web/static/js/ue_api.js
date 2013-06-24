ba086b30533e0c70ddb819a746b25ad27aec0d43
caobowei <1193693747@qq.com>
2013-06-24 14:32:37 +0800

    iou

--- web/static/js/ue_api.js
+++ web/static/js/ue_api.js
@@ -316,7 +316,7 @@ function getWeixins() {
                         }
                     });
 
-//                    digui($(button));
+                    digui($(button));
 
                 }
 
