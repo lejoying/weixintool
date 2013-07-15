2dc2bc3f1106a4be5a9c6e2914bb34c19b9bf5b1
lgagh <clgagh@gmail.com>
2013-07-15 12:34:45 +0800

    update index popmenu ue

---
+++ weihuweb/static/js/index.js
@@ -0,0 +1,27 @@
+// JavaScript Document
+$(document).ready(function(){
+	$(".js_weixinNow").click(function(){
+		$(".accountSwitching").toggle();
+		addEvent(document.body,"mousedown",clickother);
+	});
+});
+//添加点击空白处关闭弹出框事件
+function addEvent(obj,eventType,func){
+	if(obj.attachEvent){obj.attachEvent("on" + eventType,func);}
+	else{obj.addEventListener(eventType,func,false)}
+}
+function clickother(el){
+	thisObj = el.target?el.target:event.srcElement;
+	if(thisObj.id == "accountSwitching"||thisObj.id == "weixinNow"){
+		return;
+	} 
+	do{		
+		if(thisObj.tagName == "BODY"){			
+			if(document.getElementById("accountSwitching")){
+				document.getElementById("accountSwitching").style.display = "none";
+			}
+			return;
+		};
+		thisObj = thisObj.parentNode;
+	}while(thisObj.parentNode);
+}
