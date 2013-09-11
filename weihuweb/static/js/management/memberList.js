644867db6cb89d349c79200f647be2dfa440d399
coolspan <coolspan@sina.cn>
2013-09-11 11:47:22 +0800

    weihu management update

--- weihuweb/static/js/management/memberList.js
+++ weihuweb/static/js/management/memberList.js
@@ -6,11 +6,13 @@
  * To change this template use File | Settings | File Templates.
  */
 $(document).ready(function(){
-    var pagesize = 10;
+    var pagesize = 4;
     var index = 1;
     var count = 0;
-    getAllAccount(0, pagesize);
-    function getAllAccount(start, end){
+    getAllAccount(0, pagesize, count, index);
+    function getAllAccount(start, end, count, index){
+        count = count;
+        index = index;
         $.ajax({
             type: "GET",
             url: "/api2/account/getnowpageaccount?",
@@ -38,86 +40,11 @@ $(document).ready(function(){
                             });
                         });
                     });
+                    $(".js_name").each(function(i){
+                        $($(".js_name")[i]).html(unescape($($(".js_name")[i]).html()));
+                    });
                     if(count == 0){
-                        count = Math.ceil(serverData["count"]/pagesize);
-                        $($(".pagination a")[1]).hide();
-                        $($(".pagination a")[9]).attr("value",count);
-                        $(".pagination a").each(function(i){
-                            var now = 0;
-                            if($($(".pagination a")[i]).attr("value")>count){
-                                $($(".pagination a")[i]).attr("class","unclick");
-                                return true;
-                            }
-                            $($(".pagination a")[i]).click(function(){
-                                if(i != 1 && i != 8){
-                                    now = index;
-                                    index = $($(".pagination a")[i]).attr("value");
-                                }
-                                $($(".pagination a")[7]).html(Math.ceil(index/10)*10);
-                                $($(".pagination a")[7]).attr("value",(index/10)*10);
-                                if(i == 1){
-                                    now = index;
-                                    if(index-1>0){
-                                        index--;
-                                    }else{
-                                        index = 1;
-                                    }
-                                }
-                                if(i == 8){
-                                    now = index;
-                                    if(index+1<count){
-                                        index++;
-                                    }else{
-                                        index = count;
-                                    }
-                                }
-                                if(index > 1){
-                                    $($(".pagination a")[1]).show();
-                                }else{
-                                    $($(".pagination a")[1]).hide();
-                                }
-//                                alert(index);
-                                /*if(index-1<=0){
-                                 index = 1;
-                                 }
-                                 $($(".pagination a")[1]).attr("value",index-1);
-                                 $($(".pagination a")[1]).attr("value",index-1);
-                                 $($(".pagination a")[8]).attr("value",index+1);*/
-                                if(count>=5){
-                                    if(i==5 || i==6 || i == 8){
-                                        if(index<count && index>2){
-                                            for(var x=2;x<7;x++){
-                                                $($(".pagination a")[x]).html(index-4+x);
-                                                $($(".pagination a")[x]).attr("value",index-4+x);
-                                                $($(".pagination a")[x]).attr("title",index-4+x);
-                                            }
-                                        }
-                                    }
-                                    if(i==2 || i==3 || i==1){
-                                        if(index>1 && index>2){
-                                            for(var x=2;x<7;x++){
-                                                $($(".pagination a")[x]).html(index-4+x);
-                                                $($(".pagination a")[x]).attr("value",index-4+x);
-                                                $($(".pagination a")[x]).attr("title",index-4+x);
-                                            }
-                                        }
-                                    }
-                                }
-                                $(".pagination a").each(function(j){
-                                    $(".pagination a")[j].removeAttribute("class");
-                                    if($($(".pagination a")[j]).html() == index){
-                                        $($(".pagination a")[j]).attr("class","pageSelected");
-                                    }
-                                    if($($(".pagination a")[j]).attr("value")>count){
-                                        $($(".pagination a")[j]).attr("class","unclick");
-                                        return true;
-                                    }
-                                });
-                                if(now != index){
-                                    getAllAccount((index-1)*pagesize, pagesize);
-                                }
-                            });
-                        });
+                        getPageData(getAllAccount, pagesize, count, index, serverData["count"]);
                     }
                 }
             }
