d0990450cc352c00a07595a5b1ac53c434812d61
coolspan <coolspan@sina.cn>
2013-09-25 10:50:02 +0800

    weihu apiDefinition update

--- server/weixinserver/apiDefinition.js
+++ server/weixinserver/apiDefinition.js
@@ -59,8 +59,68 @@ api = {
             typical: {accountname: "XXX", accesskey: "XXXXXX", "password": "******"}
         },
         response: {
-            success: {"提示信息": "修改密码成功", account: {}},
+            success: {"提示信息": "获得分页注册用户成功", account: {}},
             failed: {"提示信息": "修改密码失败", "失败原因": "原密码不正确"}
+        }
+    },
+    /***************************************
+     * URL：/api2/account/getnowpageaccount
+     ***************************************/
+    "account_getnowpageaccount": {
+        request: {
+            typical: {start: "XXX", end: "XXXXXX"}
+        },
+        response: {
+            success: {"提示信息": "修改密码成功", accounts: {},count:"XX"},
+            failed: {"提示信息": "获得分页注册用户失败", "失败原因": "无注册用户"}
+        }
+    },
+    /***************************************
+     * URL：/api2/account/getbyid
+     ***************************************/
+    "account_getbyid": {
+        request: {
+            typical: {uid:"XX"}
+        },
+        response: {
+            success: {"提示信息": "获取用户信息成功", account: {}},
+            failed: {"提示信息": "获取用户信息失败", "失败原因": "用户信息不存在"}
+        }
+    },
+    /***************************************
+     * URL：/api2/account/modifyaccount
+     ***************************************/
+    "account_modifyaccount": {
+        request: {
+            typical: {uid:"XX",account:"XX"}
+        },
+        response: {
+            success: {"提示信息": "修改用户信息成功", account: {}},
+            failed: {"提示信息": "修改用户信息失败", "失败原因": "用户信息不存在"}
+        }
+    },
+    /***************************************
+     * URL：/api2/account/getallcount
+     ***************************************/
+    "account_getallcount": {
+        request: {
+            typical: {}
+        },
+        response: {
+            success: {"提示信息": "获得所有注册用户数量成功", count: "XX"},
+            failed: {"提示信息": "获得所有注册用户数量失败", "失败原因": "数据异常"}
+        }
+    },
+    /***************************************
+     * URL：/api2/account/delete
+     ***************************************/
+    "account_delete": {
+        request: {
+            typical: {uid:"XX"}
+        },
+        response: {
+            success: {"提示信息": "删除注册用户成功"},
+            failed: {"提示信息": "删除注册用户失败", "失败原因": "用户不存在"}
         }
     }
 }
@@ -88,7 +148,7 @@ api = {
      ***************************************/
     "weixinuer_[getall]": {
         request: {
-            typical: {uid: "nnnn", accesskey: "XXXXXX"}
+            typical: {uid: "nnnn", start: "XXXXXX", end:""}
         },
         response: {
             success: {"提示信息": "获取所有绑定微信公众账号成功", weixins: {
@@ -99,7 +159,7 @@ api = {
                 ("..."): "..."
             }
             },
-            failed: {"提示信息": "获取所有绑定微信公众账号失败", "失败原因": "没有已绑定微信公众账号"}
+            failed: {"提示信息": "获取所有绑定微信公众账号失败" || "获得所有绑定微信数量失败", "失败原因": "没有已绑定微信公众账号" || "数据格式不正确"}
         }
     },
     /***************************************
@@ -107,7 +167,7 @@ api = {
      ***************************************/
     "weixin_getbyid": {
         request: {
-            typical: {weixinid: "nnnn"}
+            typical: {weixinid: "XXX"}
         },
         response: {
             success: {"提示信息": "获取微信信息成功", weixin:{}},
@@ -143,11 +203,23 @@ api = {
      ***************************************/
     "weixin_modify": {
         request: {
-            typical: {weixinid: "XXXX", weixin: JSON({})}
+            typical: {weixinid: "XXXX", weixin: "XX"}
         },
         response: {
             success: {"提示信息": "修改绑定微信信息成功", weixin: {}},
             failed: {"提示信息": "修改绑定微信信息失败", "失败原因": "绑定微信信息不存在"}
+        }
+    },
+    /***************************************
+     * URL：/api2/weixin/delete
+     ***************************************/
+    "weixin_delete": {
+        request: {
+            typical: {weixinid: "XXXX", uid: "XX"}
+        },
+        response: {
+            success: {"提示信息": "删除绑定微信成功"},
+            failed: {"提示信息": "删除绑定微信失败", "失败原因": "数据不正常"}
         }
     },
     /***************************************
@@ -160,6 +232,42 @@ api = {
         response: {
             success: {"提示信息": "修改绑定微信开关成功", weixin: {}},
             failed: {"提示信息": "改绑定微信开关失败", "失败原因": "数据不正常"}
+        }
+    },
+    /***************************************
+     * URL：/api2/weixin/getnowpageweixin
+     ***************************************/
+    "weixin_getnowpageweixin": {
+        request: {
+            typical: {start:"XX", end:"XX"}
+        },
+        response: {
+            success: {"提示信息": "获取绑定微信公众账号分页数据成功", weixins: {}, count:"XX"},
+            failed: {"提示信息": "获取绑定微信公众账号分页数据失败" || "获得所有绑定微信数量失败", "失败原因": "没有已绑定微信公众账号" || "数据格式不正确"}
+        }
+    },
+    /***************************************
+     * URL：/api2/weixin/getnewusercount
+     ***************************************/
+    "weixin_getnewusercount": {
+        request: {
+            typical: {weixinid:"XX", time:"XX"}
+        },
+        response: {
+            success: {"提示信息": "获取今日新增会员数量成功", count:"XX"},
+            failed: {"提示信息": "获取今日新增会员数量失败", "失败原因": "数据格式不正确"}
+        }
+    },
+    /***************************************
+     * URL：/api2/weixin/getbindcount
+     ***************************************/
+    "weixin_getbindcount": {
+        request: {
+            typical: {weixinid:"XX", time:"XX"}
+        },
+        response: {
+            success: {"提示信息": "获取绑定微信数量成功", count:"XX"},
+            failed: {"提示信息": "获取绑定微信数量失败", "失败原因": "数据异常"}
         }
     }
 }
@@ -181,7 +289,7 @@ api = {
                 {id: "..", "……": "......"},
                 "..."
             ]},
-            failed: {"提示信息": "获得所有关注用户失败", "失败原因": "微信公众账号不存在"}
+            failed: {"提示信息": "获得所有关注用户失败" || "获得所有关注用户数量失败", "失败原因": "微信公众账号不存在"}
         }
     },
     /***************************************
@@ -189,7 +297,7 @@ api = {
      ***************************************/
     "user_modify": {
         request: {
-            typical: {uid: "nnnn", accesskey: "XXXXXX", userid: "XXXX", user: JSON({})}
+            typical: {userid: "XXXX", user: JSON({})}
         },
         response: {
             success: {"提示信息": "修改关注用户信息成功", user: {}},
@@ -207,6 +315,30 @@ api = {
             success: {"提示信息": "获取用户信息成功", user: {}},
             failed: {"提示信息": "获取用户信息失败", "失败原因": "用户信息不存在"}
         }
+    },
+    /***************************************
+     * URL：/api2/user/getnowpageuser
+     ***************************************/
+    "user_getnowpageuser": {
+        request: {
+            typical: {start: "XX",end: "XX"}
+        },
+        response: {
+            success: {"提示信息": "获得分页关注用户成功", users: {}, count: "XX"},
+            failed: {"提示信息": "获得分页关注用户失败" || "获得所有关注用户数量失败", "失败原因": "微信公众账号不存在" || "数据格式不正确"}
+        }
+    },
+    /***************************************
+     * URL：/api2/user/delete
+     ***************************************/
+    "user_delete": {
+        request: {
+            typical: {userid:"XX"}
+        },
+        response: {
+            success: {"提示信息": "删除用户信息成功"},
+            failed: {"提示信息": "删除用户信息失败", "失败原因": "用户信息不存在"}
+        }
     }
 }
 
@@ -220,13 +352,13 @@ api = {
      ***************************************/
     "app_getall": {
         request: {
-            typical: {uid: "nnnn", accesskey: "XXXXXX", filter: ["ALL" | "OWN" | "BIND"], _weixinOpenID: "nnnnn" }
+            typical: {uid: "nnnn", type: ["public" | "instrudy"], filter: ["ALL" | "OWN" | "BIND"], mold:"XX",status:"XX",start:"XX",end: "XX"}
         },
         response: {
             success: {"提示信息": "获得应用列表成功", apps: [
                 {id: "..", "……": "......"},
                 "..."
-            ]},
+            ],count:"XX"},
             failed: {"提示信息": "获得应用列表失败", "失败原因": "指定微信公众账号|账号不存在"}
         }
     },
@@ -264,6 +396,126 @@ api = {
         response: {
             success: {"提示信息": "删除应用成功"},
             failed: {"提示信息": "删除应用失败", "失败原因": "应用不存在"}
+        }
+    },
+    /***************************************
+     * URL：/api2/app/getbyid
+     ***************************************/
+    "app_getbyid": {
+        request: {
+            typical: {appid: "nnnn"}
+        },
+        response: {
+            success: {"提示信息": "获取应用信息成功", app:{}},
+            failed: {"提示信息": "获取应用信息失败", "失败原因": "应用信息不存在"}
+        }
+    },
+    /***************************************
+     * URL：/api2/app/addmyapp
+     ***************************************/
+    "app_addmyapp": {
+        request: {
+            typical: {weixinid: "XXX",myapp:"XXX"}
+        },
+        response: {
+            success: {"提示信息": "保存个性化设置成功", r:{}},
+            failed: {"提示信息": "保存个性化设置失败", "失败原因": "保存数据出现异常"}
+        }
+    },
+    /***************************************
+     * URL：/api2/app/getallmyapp
+     ***************************************/
+    "app_getallmyapp": {
+        request: {
+            typical: {weixinid: "XXX"}
+        },
+        response: {
+            success: {"提示信息": "获取个性化设置成功", r:{},count:"XX"},
+            failed: {"提示信息": "获取个性化设置失败", "失败原因": "获取数据出现异常"}
+        }
+    },
+    /***************************************
+     * URL：/api2/app/modifymyapp
+     ***************************************/
+    "app_modifymyapp": {
+        request: {
+            typical: {weixinid: "XXX", appid:"XX", r:"XX"}
+        },
+        response: {
+            success: {"提示信息": "获取应用信息成功", r:{}},
+            failed: {"提示信息": "获取应用信息失败", "失败原因": "应用信息不存在"}
+        }
+    },
+    /***************************************
+     * URL：/api2/app/getmyapp
+     ***************************************/
+    "app_getmyapp": {
+        request: {
+            typical: {weixinid: "XXX", appid:"XX"}
+        },
+        response: {
+            success: {"提示信息": "获取应用信息成功", r:{}},
+            failed: {"提示信息": "获取应用信息失败", "失败原因": "应用信息不存在"}
+        }
+    },
+    /***************************************
+     * URL：/api2/app/getappscount
+     ***************************************/
+    "app_getappscount": {
+        request: {
+            typical: {type: "XXX"}
+        },
+        response: {
+            success: {"提示信息": "获取应用数量成功", count:"XX"},
+            failed: {"提示信息": "获取应用数量失败", "失败原因": "数据格式不正常"}
+        }
+    },
+    /***************************************
+     * URL：/api2/app/modifystatus
+     ***************************************/
+    "app_modifystatus": {
+        request: {
+            typical: {appid: "XXX", status:"XX"}
+        },
+        response: {
+            success: {"提示信息": "修改应用信息成功", app:{}},
+            failed: {"提示信息": "修改应用信息失败", "失败原因": "应用信息不存在"}
+        }
+    },
+    /***************************************
+     * URL：/api2/app/getprivateapp
+     ***************************************/
+    "app_getprivateapp": {
+        request: {
+            typical: {start: "XXX", end:"XX"}
+        },
+        response: {
+            success: {"提示信息": "获得用户个性化设置成功", rs:{}, count:"XX"},
+            failed: {"提示信息": "获得用户个性化设置失败", "失败原因": "个性化设置不存在"}
+        }
+    },
+    /***************************************
+     * URL：/api2/app/getappcount
+     ***************************************/
+    "app_getappcount": {
+        request: {
+            typical: {}
+        },
+        response: {
+            success: {"提示信息": "获取微乎应用数量成功", oncount:"XX", offcount:"XX"},
+            failed: {"提示信息": "获取微乎应用数量失败", "失败原因": "数据异常"}
+        }
+    },
+    /***************************************
+     * URL：/api2/app/getuserappcount
+     ***************************************/
+    "app_getuserappcount": {
+        request: {
+            typical: {}
+        },
+        response: {
+            success: {"提示信息": "获取个性化应用开启数量成功", count:"XX"},
+            failed: {"提示信息": "获取微乎应用数量失败", "失败原因": "数据异常"}
         }
     }
 }
